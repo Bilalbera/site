c1e71197f5c74786f3a01dc2fc9126cb3dcedf05
Bilalbera <110243066+Bilalbera@users.noreply.github.com>
2026-07-29 18:06:21 +0300

    Update and rename index.html to main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+merhaba = "Merhaba"
+
+alinanisim = input("Merhaba! Ben BilalAI: ")
+if alinanisim == merhaba:
+    print("BilalAI: Merhaba! Bugün Nasılsın?")
+
+elif alinanisim == "merhaba":
+    print("BilalAI: Merhaba! Nasılın keyfin yerinde mi?")
+elif alinanisim == "selam":
+    print("BilalAI: Selam! Bir İsteğin Varmı?")
+elif alinanisim == "Selam":
+    print("BilalAI: Selam! Bir İstek veya Sorun Var mı?")
+elif alinanisim == "Bana basit bir python kodu yaz":
+    print("BilalAI: Tabii! İşte basit bir Python kodu örneği:\n print('Merhaba, Dünya!')")
+elif alinanisim == "Sen Kimsin?":
+    print("BilalAI: Ben bir yapay zeka asistanıyım. Sana yardımcı olmaktan memnun olurum.")
+elif alinanisim == "Kim tarafından yapıldın?":
+    print("BilalAI: Ben Bilal Bera isimli bir geliştirici tarafından python kullanarak yapıldım.")
